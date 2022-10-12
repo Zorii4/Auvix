c8bdef47cb102bbf10409f931bb38967e6ca8839
Alex Shamov <lex.shamov@gmail.com>
2022-10-12 17:39:36 +0300

    fix conflict

--- router.js
+++ router.js
@@ -13,7 +13,6 @@ export function createRouter() {
     mode: 'history',
     routes: [
       {
-<<<<<<< HEAD
         path: '/catalog/brands',
         name: 'AllBrands',
         component: AllBrands,
@@ -24,8 +23,6 @@ export function createRouter() {
         component: BrandItem,
       },
       {
-=======
->>>>>>> master
         path: '/news/:code',
         name: 'NewsPage',
         component: NewsPage,
@@ -36,8 +33,6 @@ export function createRouter() {
         component: MainPage,
       },
     ],
-<<<<<<< HEAD
-=======
     scrollBehavior(_to, _from, savedPosition) {
       return new Promise((resolve) => {
         if (savedPosition) {
@@ -49,6 +44,5 @@ export function createRouter() {
         }
       })
     },
->>>>>>> master
   })
 }
