59e574f3b3482efd359b592b4d69942c2447d39c
Anton Zorin <workzor@bk.ru>
2022-11-07 08:41:04 +0300

    add cases blocks

---
+++ components/CasesBlocks/WithTile.vue
@@ -0,0 +1,16 @@
+<template>
+  <SolutionsBlocksTileWithPagination
+    :pageProps="pageProps"
+    :solutionsData="dateSort"
+  />
+</template>
+
+<script>
+export default {
+  name: 'WithTile',
+  props: {
+    pageProps: Object,
+    dateSort: Array,
+  },
+}
+</script>
