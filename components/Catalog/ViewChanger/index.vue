3792c4fc92d4adebfc3425b26f47967524f5bc54
Alex Shamov <lex.shamov@gmail.com>
2022-11-08 11:16:55 +0300

    added inital html for changer

---
+++ components/Catalog/ViewChanger/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="catalog__content-grid-filter">
+    <button class="active grid-row-js">
+      <GridFilterRowIcon />
+    </button>
+    <button class="grid-tile-js">
+      <GridFilterTileIcon />
+    </button>
+  </div>
+</template>
+
+<script>
+import GridFilterRowIcon from '@/assets/icons/grid-filter-1.svg'
+import GridFilterTileIcon from '@/assets/icons/grid-filter-2.svg'
+export default {
+  name: 'CatalogViewChanger',
+
+  components: {
+    GridFilterRowIcon,
+    GridFilterTileIcon,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
